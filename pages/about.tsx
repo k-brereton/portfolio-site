f6553365399f2032af5135e77988c291ffa4ad80
k-brereton <kevin.brereton@ucalgary.ca>
2021-05-11 12:23:37 -0600

    added spacing to about phone and email icons

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,9 @@
-import Link from "next/link";
 import Image from "next/image";
 import {ContactMediaBar, ICON_SIZE, PageHead} from "../components/CommonComponents";
 
 // @ts-ignore
 import style from "./about.module.scss"
-import {RiMailFill, RiPhoneFill} from "react-icons/ri";
+import {RiPhoneFill} from "react-icons/ri";
 import {HiOutlineMail} from "react-icons/hi";
 import {Container, Row} from "react-bootstrap";
 
@@ -24,13 +23,11 @@ export default function  About() {
                     Hi, I am a freshly graduated software developer from Calgary, Alberta. I am passionate about coding, and design principles. While I have stellar grades and impressive projects, my largest source of experience is from the year I spent on internship developing applications for one of the National Bank of Canada's hedge funds. I am also an avid chess player, and fan of cats.
                 </div>
                 <div className={style.contactArea}>
-                    {/*email/phone basic obfuscation. Nothing complex but good enough for 99% of spammers*/}
-                    <Row><RiPhoneFill size={ICON_SIZE}/> 403 <span className="hidden"> plz no spam</span>- 400 <span className="hidden"> plz</span> - 0901 </Row>
-                    <Row><HiOutlineMail size={ICON_SIZE}/> kevin<span className="hidden"> kldas;jfa</span>.brereton1<span className="hidden"> daaasc</span>@gmail<span className="hidden"> .ca</span>.com </Row>
+                    {/*email/phone basic obfuscation. Nothing complex but good enough for most spammers*/}
+                    <Row><RiPhoneFill size={ICON_SIZE} className={style.aboutContactIcon}/> 403 <span className="hidden"> plz no spam</span>- 400 <span className="hidden"> plz</span> - 0901 </Row>
+                    <Row><HiOutlineMail size={ICON_SIZE} className={style.aboutContactIcon}/> kevin<span className="hidden"> kldas;jfa</span>.brereton1<span className="hidden"> daaasc</span>@gmail<span className="hidden"> .ca</span>.com </Row>
                 </div>
             </Container>
         </div>
-
-
     </>
 }
