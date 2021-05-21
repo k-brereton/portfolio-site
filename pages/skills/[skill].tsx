ea2265515abfe4ae60aa1fb9ee9cb87b007eef84
k-brereton <kevin.brereton@ucalgary.ca>
2021-05-21 12:53:29 -0600

    Fixed up individual skill page overall layout

--- pages/skills/[skill].tsx
+++ pages/skills/[skill].tsx
@@ -1,15 +1,18 @@
 import {ALL_PROJECT_TAGS, PROJECT_DATA, ProjectComponent, ProjectTag} from "../../components/ExperienceComponents"
 import {InitialPageAnimations, PageHead} from "../../components/CommonComponents";
 import {AnimateSharedLayout} from "framer-motion";
+import {Container} from "react-bootstrap";
 
 export default function SkillComponent({skill}:{skill:ProjectTag}) {
     const dataToShow=PROJECT_DATA.filter(data=>data.tags.includes(skill))
         .map((data,index)=>(<ProjectComponent key={index} projectData={data}/>));
 
     return <InitialPageAnimations>
-        <PageHead  description={`Kevin Brereton's experience with ${skill}`} title={`Kevin Brereton | ${skill}`}/>
-        <h1>{skill}</h1>
-        <AnimateSharedLayout>{dataToShow}</AnimateSharedLayout>
+        <PageHead description={`Kevin Brereton's experience with ${skill}`} title={`Kevin Brereton | ${skill}`}/>
+        <Container>
+            <h1 className="display-1 mainTitle">{skill} Experience</h1>
+            <AnimateSharedLayout>{dataToShow}</AnimateSharedLayout>
+        </Container>
     </InitialPageAnimations>
 
 }
